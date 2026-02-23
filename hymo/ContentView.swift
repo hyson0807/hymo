f3b6f47c16395017677c3badbc6f95f629ad6085
hyson0807 <simsgood0807@gmail.com>
2026-02-23 13:08:22 +0900

    Initial Commit

---
+++ hymo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hymo
+//
+//  Created by hyson on 2/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hymo/hymoApp.swift
@@ -0,0 +1,17 @@
+//
+//  hymoApp.swift
+//  hymo
+//
+//  Created by hyson on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct hymoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
